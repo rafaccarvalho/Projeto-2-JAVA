86d304d5c17dfb4db4af35fadec0f54eee9fa74a
rafaccarvalho <rafacarvalhocordeiro@gmail.com>
2025-05-20 19:45:38 -0300

    Update Mesa.java

--- Projeto pizzaria/Mesa.java
+++ Projeto pizzaria/Mesa.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 public class Mesa {
     private int numero;
     private int capacidade;
+    private boolean ocupada; 
 
     public Mesa(int numero, int capacidade) {
         this.numero = numero;
         this.capacidade = capacidade;
+        this.ocupada = false;
     }
 
     public int getNumero() {
@@ -18,18 +20,35 @@ public class Mesa {
         return capacidade;
     }
 
-    // Novo método para encontrar a melhor mesa
+    public boolean isOcupada() {
+        return ocupada;
+    }
+
+    public void ocupar() {
+        this.ocupada = true;
+    }
+
+    public void liberar() {
+        this.ocupada = false;
+    }
+
+    // método para encontrar a melhor mesa
     public static Mesa alocarMesa(ArrayList<Mesa> mesas, int quantidadePessoas) {
         Mesa melhorMesa = null;
 
         for (Mesa mesa : mesas) {
-            if (mesa.getCapacidade() >= quantidadePessoas && quantidadePessoas >= mesa.getCapacidade() * 0.5) {
+            // Verifica se a mesa está desocupada e se ela é adequada
+            if (!mesa.isOcupada() && mesa.getCapacidade() >= quantidadePessoas && quantidadePessoas >= mesa.getCapacidade() * 0.5) {
                 if (melhorMesa == null || mesa.getCapacidade() < melhorMesa.getCapacidade()) {
                     melhorMesa = mesa;
                 }
             }
         }
 
+        if (melhorMesa != null) {
+            melhorMesa.ocupar();
+        }
+
         return melhorMesa;
     }
-}          
+}
